83c6875c873f4928e451002a37498bc734aec905
Samuk515 <singhsamir515@gmail.com>
2026-02-24 20:11:37 +0545

    update

---
+++ Evenhundred.java
@@ -0,0 +1,9 @@
+public class Evenhundred {
+   public static void main(String[] args) {
+    for (int i = 0; i <=100; i++) {
+       if(i%17 == 0) System.out.print(i+ " ");
+        
+    }
+   } 
+    
+}
