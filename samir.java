7ae4f02eba5699f62fa135ebd0c85850d088de0f
Samuk515 <singhsamir515@gmail.com>
2026-03-11 19:22:34 +0545

    YT

--- samir.java
+++ samir.java
@@ -2,19 +2,19 @@
 import java.util.Scanner;
 
 public class samir {
-    public static void main(String[] args) {
-        Scanner Samir = new Scanner(System.in);
-        System.out.print("Enter the first number : ");
-        int saunk = Samir.nextInt();
-        System.out.print("Enter the second number : ");
-        int ram = Samir.nextInt();
-        System.out.print("Enter the third number : ");
-        int pramod = Samir.nextInt();
-        System.out.println("The sum of three number is : " + (saunk+ram+pramod));        
-
-        
-        
-        }
-
+    public static int calculateAdd(int a, int b){
+        int sam = a +b;
+        return sam;
     }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
 
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
+        
+
+        System.out.println("The sum of the two numbers is: "+ calculateAdd(a, b));
+    }
+}
